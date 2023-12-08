b929d5e230c4b4547fd8d44b01379a9b7a4a77b4
asda488 <yoshicys@gmail.com>
2023-12-08 14:40:49 +0000

    small breaking change, start db

--- Models/Database.cs
+++ Models/Database.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
-using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
 
 namespace MusicPlayer.Models {
     class Database {
-        /*
         public (List<Song>, List<Playlist>) LoadData(){
-            SqlConnection connection;
+            using (SqliteConnection connection = new SqliteConnection("Data Source=Assets/music.db")){
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = @"SELECT * FROM Songs";
+            }
+
         }
-        */
     }
 }
