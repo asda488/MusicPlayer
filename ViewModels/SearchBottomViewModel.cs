636481f1df98393b5a4504dd6e87234e238a5e22
asda488 <yoshicys@gmail.com>
2024-02-01 09:58:01 +0000

    start documentation

--- ViewModels/SearchBottomViewModel.cs
+++ ViewModels/SearchBottomViewModel.cs
@@ -1,6 +1,12 @@
+using MusicPlayer.Models;
+using System.Collections.Generic;
+using System.Windows.Input;
 namespace MusicPlayer.ViewModels;
 
 public class SearchBottomViewModel : ViewModelBase {
+    public ICommand NewPlaylistSong { get => new(); }
+    public List<SearchItem> SearchResults { get => searchResults;}
+    public List<SearchItem> searchResults;
     public SearchBottomViewModel(){
         
     }
